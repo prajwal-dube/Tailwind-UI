b6cc4352ccdd40e6a0c33df8b8d4133cd29cdafc
Prajwal-Dube <prajwal.dube12@gmail.com>
2025-10-16 12:59:59 +0530

    Tailwind_UI

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],   
+}
+
+// In content array we have to add 
+// the path of all the files where 
+// we are going to use tailwind css 
+// classes
+// If we want to use in all the files
